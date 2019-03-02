0dc6074c941caa418b7fda3258b8b753605f6f4a
Thomas Ghysels <info@thomasg.be>
2019-03-02 22:35:05 +0100

    v0.21.0

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # now-sapper
-Builder 
+
+Your Sapper project must be adapted to work with Zeit Now v2, see https://github.com/thgh/sapper-template/commit/220307c800525633063df3e3373bc76d0e62cd86
+
+For Express, the instance must be exported in `src/server.js`
+```
+const app = express()
+export default app
+```
+
+For Polka, the handler must be exported in `src/server.js`
+```
+const app = polka()
+export default app.handler
+```
+
+Example `now.json`
+```
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "__sapper__/build/index.js",
+      "use": "now-sapper"
+    }
+  ],
+  "routes": [{ "src": "/(.*)", "dest": "__sapper__/build/index.js" }]
+}
+```
+
+Recommended ignore:
+```
+# .nowignore
+*
+!static
+!static/
+!__sapper__
+!__sapper__/
+__sapper__/dev
+```
---
+++ index.js
@@ -0,0 +1,79 @@
+const { createLambda } = require('@now/build-utils/lambda.js') // eslint-disable-line import/no-extraneous-dependencies
+const path = require('path')
+const rename = require('@now/build-utils/fs/rename.js') // eslint-disable-line import/no-extraneous-dependencies
+
+const FileBlob = require('@now/build-utils/file-blob.js');
+const FileFsRef = require('@now/build-utils/file-fs-ref.js');
+const fs = require('fs-extra');
+
+const { spawn } = require('child_process');
+function spawnAsync(command, args, cwd) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, { stdio: 'inherit', cwd });
+    child.on('error', reject);
+    child.on('close', (code, signal) => (code !== 0
+      ? reject(new Error(`Exited with ${code || signal}`))
+      : resolve()));
+  });
+}
+
+exports.config = {
+  maxLambdaSize: '10mb'
+}
+
+exports.build = async ({ files, entrypoint, workPath }) => {
+  // move all user code to 'user' subdirectory
+  const userFiles = rename(files, name => path.join('user', name))
+
+  const userPath = path.join(workPath, 'user');
+  //await spawnAsync('npm', ['install', '--only=prod'], userPath);
+
+  // Get launcher
+  const launcherFiles = {
+    'launcher.js': new FileBlob({
+      data:
+        `
+const { Server } = require('http');
+const { Bridge } = require('./bridge.js');
+
+const bridge = new Bridge();
+bridge.port = 3000;
+let listener;
+
+try {
+  process.env.PORT = bridge.port
+  if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'production';
+  }
+
+  process.chdir("./user")
+
+  listener = require("./${path.join(
+    'user',
+    '__sapper__/build/server/server.js'
+  )}");
+
+  if (listener.default) {
+    listener = listener.default;
+  }
+} catch (error) {
+  bridge.userError = error;
+}
+
+const server = new Server(listener);
+server.listen(bridge.port);
+
+exports.launcher = bridge.launcher;
+`
+    }),
+    'bridge.js': new FileFsRef({ fsPath: require('@now/node-bridge') })
+  }
+
+  const lambda = await createLambda({
+    files: { ...userFiles, ...launcherFiles },
+    handler: 'launcher.launcher',
+    runtime: 'nodejs8.10'
+  })
+
+  return { [entrypoint]: lambda }
+}
