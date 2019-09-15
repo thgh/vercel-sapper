a1c1f4ba6c82e6762dcbf1c29362848525ef5df4
Antony Jones <ant@enzy.org>
2019-09-15 08:39:04 +0100

    Significant refactoring / modernisation (#7)
    * refactor and modernise
    
    * remove node_modules
    
    * reorganise
    
    * debugging
    
    * workaround for now dev
    
    * cleanup
    
    * improve readme

--- README.md
+++ README.md
@@ -1,5 +1,65 @@
 # now-sapper
 
+## Usage
+
+### Basic usage
+
+Pushes the source to now, and builds the project.
+
+Recommended `.nowignore`:
+```
+__sapper__
+cypress
+node_modules
+```
+
+Example `now.json`
+```json
+{
+  "version": 2,
+    "builds": [
+    { "src": "package.json", "use": "now-sapper" }
+  ],
+}
+```
+
+### No-build usage
+
+Useful if you are building the project on CI, and then want to just push the compiled source.
+
+Recommended `.nowignore`:
+```
+__sapper__/dev
+__sapper__/export
+cypress
+node_modules
+```
+
+Example `now.json`
+```json
+{
+  "version": 2,
+    "builds": [
+    { "src": "package.json", "use": "now-sapper", { build: false } }
+  ]
+}
+```
+
+### Changing the node runtime
+
+You can change the AWS lambda runtime from the default node 8
+
+Example `now.json`
+```json
+{
+  "version": 2,
+    "builds": [
+    { "src": "package.json", "use": "now-sapper", { runtime: 'nodejs10.x' } }
+  ]
+}
+
+## Preparation
+
 Your Sapper project must be adapted to work with Zeit Now v2, see https://github.com/thgh/sapper-template/commit/220307c800525633063df3e3373bc76d0e62cd86
 
 For Express, the instance must be exported in `src/server.js`
@@ -12,38 +72,4 @@ For Polka, the handler must be exported in `src/server.js`
 ```js
 const app = polka()
 export default app.handler
-```
-
-Example `now.json`
-```json
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "__sapper__/build/index.js",
-      "use": "now-sapper"
-    }
-  ],
-  "routes": [{ "src": "/(.*)", "dest": "__sapper__/build/index.js" }]
-}
-```
-
-Recommended `.nowignore`:
-```
-__sapper__/dev
-__sapper__/export
-cypress
-node_modules
-src
-```
-
-Previous recommended `.nowignore`
-```bash
-*
-!static
-!static/
-!__sapper__
-!__sapper__/
-!__sapper__/build
-__sapper__/dev
 ```
--- index.js
+++ index.js
@@ -1,89 +1,46 @@
 const { createLambda } = require('@now/build-utils/lambda.js') // eslint-disable-line import/no-extraneous-dependencies
 const path = require('path')
-const rename = require('@now/build-utils/fs/rename.js') // eslint-disable-line import/no-extraneous-dependencies
+const {
+  getLauncherFiles,
+  getMountPoint,
+  globAndPrefix
+} = require('./lib/utils')
 
-const FileBlob = require('@now/build-utils/file-blob.js');
-const FileFsRef = require('@now/build-utils/file-fs-ref.js');
-const fs = require('fs-extra');
-
-const { spawn } = require('child_process');
-function spawnAsync(command, args, cwd) {
-  return new Promise((resolve, reject) => {
-    const child = spawn(command, args, { stdio: 'inherit', cwd });
-    child.on('error', reject);
-    child.on('close', (code, signal) => (code !== 0
-      ? reject(new Error(`Exited with ${code || signal}`))
-      : resolve()));
-  });
-}
+const { getConfig } = require('./lib/config')
+const { npmBuild } = require('./lib/npm')
+const { download } = require('@now/build-utils')
 
 exports.config = {
   maxLambdaSize: '10mb'
 }
 
-exports.build = async ({ files, entrypoint, workPath }) => {
-  // move all user code to 'user' subdirectory
-  const basePath = path.dirname(entrypoint)
-  const userFiles = rename(files, name => path.join('user', name))
-  const userPath = path.join(workPath, 'user');
-  
-  //await spawnAsync('npm', ['install', '--only=prod'], userPath);
+exports.build = async ({ files, entrypoint, workPath, config: rawConfig, meta = {} }) => {
+  const mountpoint = getMountPoint(entrypoint)
+  const entrypointDir = path.join(workPath, mountpoint)
+  await download(files, workPath, meta)
 
-  // Get launcher
-  const launcherFiles = {
-    'launcher.js': new FileBlob({
-      data:
-        `
-const { Server } = require('http');
-const { Bridge } = require('./bridge.js');
-const fs = require('fs');
-const path = require('path');
+  process.chdir(entrypointDir)
 
-const bridge = new Bridge();
-bridge.port = 3000;
-let listener;
+  const config = getConfig(rawConfig)
+  const prodDependencies = await npmBuild(config, entrypointDir)
 
-try {
-  process.env.PORT = bridge.port
-  if (!process.env.NODE_ENV) {
-    process.env.NODE_ENV = 'production';
-  }
-
-  const rootDir = path.join(
-    process.cwd(),
-    'user',
-    "${basePath}",
-    '..',
-    '..'
-  )
-  process.chdir(rootDir)
-
-  listener = require(path.join(
-    rootDir,
-    '__sapper__/build/server/server.js'
-  ));
-
-  if (listener.default) {
-    listener = listener.default;
-  }
-} catch (error) {
-  bridge.userError = error;
-}
-
-const server = new Server(listener);
-server.listen(bridge.port);
-
-exports.launcher = bridge.launcher;
-`
-    }),
-    'bridge.js': new FileFsRef({ fsPath: require('@now/node-bridge') })
-  }
+  const launcherFiles = getLauncherFiles(mountpoint)
+  const staticFiles = await globAndPrefix(entrypointDir, 'static')
+  const applicationFiles = await globAndPrefix(entrypointDir, '__sapper__')
 
   const lambda = await createLambda({
-    files: { ...userFiles, ...launcherFiles },
+    files: { ...staticFiles, ...launcherFiles, ...prodDependencies, ...applicationFiles },
     handler: 'launcher.launcher',
-    runtime: 'nodejs8.10'
+    runtime: config.runtime
   })
 
-  return { [entrypoint]: lambda }
+  const output = { 
+    index: lambda
+  }
+
+  const routes = [
+    { src: '/(.*)', dest: '/' }
+  ]
+  
+  return { output, routes }
 }
